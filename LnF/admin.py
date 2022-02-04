123a214cde2ab6a7cfb3e3ce699ec5a7e578f883
YongbinKim <jinnem9025@daum.net>
2022-02-04 18:35:57 +0900

    [RESET]

--- LnF/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import LnF_Post, Comment
-
-# Register your models here.
-admin.site.register(LnF_Post)
-admin.site.register(Comment)
--- LnF/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LnfConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LnF'
