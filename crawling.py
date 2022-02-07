e0622687c62f36b94dba73dc2eb85ecaf222b5c5
YongbinKim <jinnem9025@daum.net>
2022-02-07 16:35:03 +0900

    [FEAT] Crawling + enroll Booth model

--- crawling.py
+++ crawling.py
@@ -1,11 +1,16 @@
-import pandas as pd
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "config.settings")
+import django
+django.setup()
+
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-
+from map.models import Booth
+from brand.models import Brand
 # 드라이버 세팅
 def set_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
@@ -30,18 +35,19 @@ def search(brand):
 def store_booth(booths_list, brand, brand_list):
     for booth in booths_list:
         if brand in booth.find_element(By.CSS_SELECTOR, "div.head_item.clickArea > strong > a.link_name").text:
-            brand_list.append(
-                {
-                    "name": booth.find_element(By.CSS_SELECTOR, "div.head_item.clickArea > strong > a.link_name").text,
-                    "location": booth.find_element(By.CSS_SELECTOR, "div.info_item > div.addr > p:nth-child(1)").text,
-                    "operationHour": booth.find_element(By.CSS_SELECTOR, "div.info_item > div.openhour > p > a").text,
+            name = booth.find_element(By.CSS_SELECTOR, "div.head_item.clickArea > strong > a.link_name").text
+            location = booth.find_element(By.CSS_SELECTOR, "div.info_item > div.addr > p:nth-child(1)").text
+            operationHour= booth.find_element(By.CSS_SELECTOR, "div.info_item > div.openhour > p > a").text
+            brand_list[name] = {
+                    "location": location,
+                    "operationHour": operationHour,
                     "brand": brand
                 }
-            )
 
 
 # crawling
-def crawling(brand, brand_list):
+def crawling(brand):
+    brand_dict = {}
     Flag = True
     idx = 1
     while Flag:
@@ -49,80 +55,69 @@ def crawling(brand, brand_list):
         if idx == 1:
             if "HIDDEN" in driver.find_element(By.ID, "info.search.place.more").get_attribute("class"):
                 booths_list = driver.find_elements(By.CLASS_NAME, "PlaceItem")
-                store_booth(booths_list, brand, brand_list)
+                store_booth(booths_list, brand, brand_dict)
                 Flag = False
             else:
                 booths_list = driver.find_elements(By.CLASS_NAME, "PlaceItem")
-                store_booth(booths_list, brand, brand_list)
+                store_booth(booths_list, brand, brand_dict)
                 driver.find_element(By.ID, "info.search.place.more").click()
                 idx += 1
 
         # next btn
         elif idx % 5 == 0:
             booths_list = driver.find_elements(By.CLASS_NAME, "PlaceItem")
-            store_booth(booths_list, brand, brand_list)
+            store_booth(booths_list, brand, brand_dict)
             driver.find_element(By.ID, "info.search.page.next").click()
             idx += 1
 
         # 종료 조건 -> 다음 번호에 hidden이라는 class 있으면 종료
         elif "HIDDEN" in driver.find_element(By.ID, "info.search.page.no" + str(idx % 5 + 1)).get_attribute("class"):
             booths_list = driver.find_elements(By.CLASS_NAME, "PlaceItem")
-            store_booth(booths_list, brand, brand_list)
+            store_booth(booths_list, brand, brand_dict)
             Flag = False
 
         else:
             booths_list = driver.find_elements(By.CLASS_NAME, "PlaceItem")
-            store_booth(booths_list, brand, brand_list)
+            store_booth(booths_list, brand, brand_dict)
             driver.find_element(By.ID, "info.search.page.no" + str(idx % 5 + 1)).click()
             idx += 1
 
         time.sleep(1)
 
-    return brand_list
+    return brand_dict
 
 
 def main():
-    # 인생네컷
-    lifefourcut_list = []
-    search("인생네컷")
-    lifefourcut_list = crawling("인생네컷", lifefourcut_list)
-    df = pd.DataFrame(lifefourcut_list)
-    df.to_csv("booth_data\lifefourcut.csv")
+    brand_list = ["인생네컷", "포토이즘", "포토시그니처", "셀픽스"]
 
-    # 포토이즘
-    photoism_list = []
-    search("포토이즘")
-    photoism_list = crawling("포토이즘", photoism_list)
-    df = pd.DataFrame(photoism_list)
-    df.to_csv("booth_data\photoism.csv")
+    brand_dict = {"인생네컷": "lifefourcut", "포토이즘": "photoism", "포토시그니처": "photosignature", "셀픽스": "selfix"}
+    logic_search = "search(brand)"
+    logic_crawling = "globals()['{}_dict'.format(eng)] = crawling(brand)"
 
-    # 포토시그니처
-    photosignature_list = []
-    search("포토시그니처")
-    photosignature_list = crawling("포토시그니처", photosignature_list)
-    df = pd.DataFrame(photosignature_list)
-    df.to_csv("booth_data\photosignature.csv")
+    for brand, eng in brand_dict.items():
+        exec(logic_search)
+        exec(logic_crawling)
 
-    # 셀픽스
-    selfix_list = []
-    search("셀픽스")
-    selfix_list = crawling("셀픽스", selfix_list)
-    df = pd.DataFrame(selfix_list)
-    df.to_csv("booth_data\selfix.csv")
+# {eng_name} = {boothname: {location: , operation_hour: , brand: }}
 
-    # # 폴라스튜디오
-    # polarstudio_list = []
-    # search("폴라스튜디오")
-    # polarstudio_list = crawling("폴라스튜디오", polarstudio_list)
-    # df = pd.DataFrame(polarstudio_list)
-    # df.to_csv("booth_data\polarstudio.csv")
+    brand_dict_list = [lifefourcut_dict, photoism_dict, photosignature_dict, selfix_dict]
+    for dict in brand_dict_list:
+        for key, value in dict.items():
+            brand = Brand.objects.get(name = value['brand'])  
+            if value['operationHour'] != '':
+                key = Booth(name = key, location = value['location'], operationHour = value['operationHour'], brand = brand)
+                key.save()
+            else:
+                key = Booth(name = key, location = value['location'], brand = brand)
+                key.save()
+
 
 
 driver = set_chrome_driver()
 driver.implicitly_wait(3)
 
 main()
-
+driver.close()
 
 
 
--- map/urls.py
+++ map/urls.py
@@ -4,9 +4,5 @@ from .import views
 app_name = "map"
 
 urlpatterns = [
-    path('', view=views.shop_list, name='list'),  # 리뷰 리스트 페이지
-    path('<int:pk>/', view=views.shop_detail, name='detail'),  # 리뷰 디테일 페이지
-    path('review/', view=views.review_create, name='create'),  # 리뷰 작성/수정 페이지
-    path('<int:pk>/update', view=views.review_update, name='update'),
-    path('<int:pk>/delete', view=views.review_delete, name='delete'),
+
 ]
