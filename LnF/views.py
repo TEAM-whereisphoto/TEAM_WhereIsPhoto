e55bf83722ea076063f9af8c2dd8e901e08b94e4
YongbinKim <jinnem9025@daum.net>
2022-02-09 21:16:24 +0900

    [FEAT] LnF 검색필터

--- LnF/views.py
+++ LnF/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import *
+from map.models import Booth
 # Create your views here.
 
 
 def list(request):
     posts=LnF_Post.objects.all()
+    query = request.GET.get('query', '')
+    if query:
+        booths = Booth.objects.filter(name__icontains = query)
+        posts = LnF_Post.objects.filter(booth__in = booths)
+            
     ctx = {'posts': posts}
     return render(request, 'LnF/list.html', context=ctx)
