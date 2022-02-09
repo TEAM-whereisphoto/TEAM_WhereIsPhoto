271081794d38d11f4cf0b64889ef8b0ad943e44f
yubin <garmalhy1@gmail.com>
2022-02-09 15:36:52 +0900

    [FEAT] 회원 탈퇴 (임시)

--- user/urls.py
+++ user/urls.py
@@ -15,23 +15,5 @@ urlpatterns = [
     path("change_password/", views.change_password, name='change_password'),
     path('member_modify/', views.member_modify, name='member_modify'),
 
-    # 보류
-    # path(
-    #     'password_reset/',
-    #     auth_views.PasswordResetView.as_view(
-    #         template_name='user/reset_password.html',
-    #         success_url=reverse_lazy('user:password_reset_done')
-    #     ),
-    #     name='password_reset'
-    # ),
-    # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='user/reset_password_sent.html'), name='password_reset_done'),
-    # path(
-    #     'password_reset_confirm/<uidb64>/<token>/',
-    #     auth_views.PasswordResetConfirmView.as_view(
-    #         template_name='user/reset_password_form.html',
-    #         success_url=reverse_lazy('user:password_reset_complete')
-    #     ),
-    #     name='password_reset_confirm'
-    # ),
-    # path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/reset_password_sucess.html'), name='password_reset_complete'),
+    path('member_del/', views.member_del, name='member_del'),
 ]
