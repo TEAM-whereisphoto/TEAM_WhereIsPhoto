5386196f036782449624963da7dcd5c5bedfe514
YongbinKim <jinnem9025@daum.net>
2022-02-09 23:58:40 +0900

    [FIX] 사진 업로드 오류

--- LnF/views.py
+++ LnF/views.py
@@ -30,7 +30,7 @@ def list(request):
 def new(request):
     user = request.user
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             new_post = form.save(commit=False)
             new_post.user = user
