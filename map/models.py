d3aa6d5a324329227a2f01c65cd18569b02cd93d
hyoung <ycat01@naver.com>
2022-02-10 00:24:45 +0900

    [FEAT] review + detail 오류0

--- map/models.py
+++ map/models.py
@@ -11,17 +11,23 @@ from user.models import User
 class Booth(models.Model):
     name = models.CharField(max_length=100)
     location = models.TextField()
-    operationHour = models.TimeField()
+    #operationHour = models.TimeField()
+    operationHour = models.CharField(max_length=50)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     user = models.ManyToManyField(User, through='Liked', through_fields=('booth', 'user'))
+    rate = models.FloatField(default= 0, null=True)
+    img = models.ImageField(blank=True, null=True, upload_to="")
+    #user = models.OneToOneField(User, through='Liked', through_fields=('booth', 'user'), on_delete=models.SET_DEFAULT, default=None)
+
 
     def __str__(self):
         return self.name + "("+str(self.brand)+")"
 
-
 # user - liked - booth 다 대 다 연결
 class Liked(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #user = models.OneToOneField(User, on_delete=models.SET_DEFAULT, default=None)
+
     booth = models.ForeignKey(Booth, on_delete=models.CASCADE)
     date = models.DateField()
 
@@ -29,11 +35,22 @@ class Liked(models.Model):
 # 리뷰 작성할 부스, 리뷰 작성하는 user, title, 리뷰 작성한 시간, 사진, 별점
 # tag 추가해야 됨
 class Review(models.Model):
-    # IRON_CHOICES = (('YES', 'yes'), ('No', 'no')),
+    IRON_CHOICES = (('YES', 'yes'), ('No', 'no'))
+    STREET_CHOICES = (('STORE', 'store'), ('ROAD', 'road'))
+    DECO_CHOICES = (('GLASS', 'glass'), ('BAND', 'band'), ('HAT', 'hat'))
+    BOXNUM_CHOICES = (('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),('5_more', '5_more'))
+
     booth = models.ForeignKey(Booth, on_delete=models.CASCADE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    #user = models.OneToOneField(User, on_delete=models.SET_DEFAULT, default=None)
+    #title = models.CharField(max_length=100)
     date = models.DateField()
     img = models.ImageField(blank=True, null=True, upload_to="")
     rate = models.IntegerField()
-    # iron = models.CharField(choices=IRON)
+    iron = models.CharField(choices=IRON_CHOICES, max_length=50)
+    street = models.CharField(choices=STREET_CHOICES, max_length=50)
+    deco = models.CharField(choices=DECO_CHOICES, max_length=50)
+    boxnum = models.CharField(choices=BOXNUM_CHOICES, max_length=50)
+
+    new = models.BooleanField(default=False)
+
--- map/views.py
+++ map/views.py
@@ -2,14 +2,39 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ReviewForm
 from .models import *
 from django.templatetags.static import static
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
 def mymap(request):
     booths = Booth.objects.all() 
-    ctx = {'booths':booths} # 너무 많으면 여기서 booths[:10] 로 몇개만 뽑아도 됨!
+    ctx = {'booths': booths} # 너무 많으면 여기서 booths[:10] 로 몇개만 뽑아도 됨!
     return render(request, 'map/mymap.html', context=ctx)
+
+def avg(pk): # 평균 별점 계산 함수
+    booth = Booth.objects.get(id=pk)
+    reviews = Review.objects.filter(booth = booth.pk)
+
+    n=0
+    sum =0
+    for review in reviews:
+        n += 1
+        sum += review.rate
+    booth.rate = sum/n
+    booth.save()
+    return n
+
+def booth_detail(request,pk):
+    booth = Booth.objects.get(id=pk)  # id가 pk인 게시물 하나를 가져온다.
+    #review = Review.objects.get(booth = booth.name)
+    reviews = Review.objects.filter(booth = booth.pk)
+    #lnfs = LnF_Post.objects.filter(booth= booth.pk)
+    #n = avg(pk)
+    ctx = {'booth': booth, 'reviews': reviews}
+    return render(request, template_name='map/booth_detail.html', context=ctx)
 
 def review_list(request):
     reviews = Review.objects.all()
@@ -19,7 +44,6 @@ def review_list(request):
 def review_detail(request, pk):  # request도 받고 몇번 인덱스인지 = pk를 받는다. 게시물 상세조
     review = Review.objects.get(id=pk)  # id가 pk인 게시물 하나를 가져온다
     ctx = {'post': review}  # template로 보내기 위해선 context를 만들어야한다.
-
     return render(request, template_name='map/review_detail.html', context=ctx)
 
 def review_create(request):
@@ -27,11 +51,10 @@ def review_create(request):
         form = ReviewForm(request.POST)
         if form.is_valid():
             post = form.save()
-            return redirect('reviews:list')
+            return redirect('reviews:review_list')
     else:
         form = ReviewForm()
         ctx = {'form': form}
-
         return render(request, template_name='map/review_form.html', context=ctx)
 
 def review_update(request, pk):
@@ -41,7 +64,8 @@ def review_update(request, pk):
         form = ReviewForm(request.POST, instance=review)
         if form.is_valid():
             review = form.save()
-            return redirect('reviews:detail', pk)
+            consol.log(ddd)
+            return redirect('reviews:review_detail', pk)
     else:
         form = ReviewForm(instance=review)
         ctx = {'form': form}
@@ -51,4 +75,26 @@ def review_update(request, pk):
 def review_delete(request, pk):
     review = get_object_or_404(Review, id=pk)
     review.delete()
-    return redirect('reviews:list')
+    return redirect('reviews:review_list')
+
+
+'''
+@csrf_exempt
+def like_ajax(request):
+    req = json.loads(request.body)
+    booth_id = req['id']
+    booth = Booth.objects.get(id =booth_id)
+
+    if booth.dolike == True:
+        post.dolike = False
+        status = post.dolike
+        k=1
+    else:
+        post.dolike = True
+        status = post.dolike
+        k=0
+    post.save()
+
+    return JsonResponse({'id': post_id, 'k': k, 'status':status})
+    
+'''
