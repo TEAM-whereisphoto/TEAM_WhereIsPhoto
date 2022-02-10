d771b5eff6c91bd0637355b83d967e046d2d0be7
Dasom Ahn <lizzy0521@naver.com>
2022-02-10 14:32:15 +0900

    [REFACTOR] map js 코드 정리

--- static/js/map/map.js
+++ static/js/map/map.js
@@ -1,35 +1,21 @@
-// 지도 초기 설정
+// 지도 자체 초기 설정
 var container = document.getElementById('map');
 var options = {
     center: new kakao.maps.LatLng(37.557074, 126.929276), // 임의의 중심 좌표
     level: 4 // 확대 축소 정도
 };
 
-
 var map = new kakao.maps.Map(container, options); // 지도 생성
-// console.log("왜 얘가 나중에되지?")
-var bounds = map.getBounds();
+var bounds = map.getBounds(); // 지도 범위 가져오는 bounds 변수 초기값 생성
 
-
-// 지도 가장자리 꾸미기 관련 ------------------------------------------------------
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+// 지도 확대 축소 컨트롤 생성
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.LEFT);
 
-// 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
-kakao.maps.event.addListener(map, 'zoom_changed', function() {        
-    
-    // 지도의 현재 레벨을 얻어옵니다
-    var level = map.getLevel();
-    
-    var message = '현재 지도 레벨은 ' + level + ' 입니다';
-    var resultDiv = document.getElementById('result');  
-    resultDiv.innerHTML = message;
-    
-});
-
 
 // blue pin으로 된 marker image 생성
+// 브랜드별 색깔 바꿀 때 이 부분 src 수정, 혹은 실제 pin 박을 때 수정도 가능
+// 참고 -> 이 api에서는 href 링크나 실제 이미지로만 pin 이미지 설정 가능. <i> rexicon꺼 </i> 등 형태 불가. 
 var imageSrc = '../static/icons/pin_blue.png'
 var imageSize = new kakao.maps.Size(32, 32);
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
@@ -48,10 +34,8 @@ gps_check();
 // 만약 작동되지 않는다면 경고창을 띄우고, 에러가 있다면 errorHandler 함수를 불러온다.
 // timeout을 통해 시간제한을 둔다.
 function gps_check(){
-    console.log("일단 이거 gps check 했고")
 
     if (navigator.geolocation) {
-        console.log("이걸 안띄우?나?")
         var options = {timeout:60000};
         navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
     } else {
@@ -60,29 +44,25 @@ function gps_check(){
     }
 }
 
-
 // gps 이용 가능 시, 위도와 경도를 반환하는 showlocation함수.
 function showLocation(position) {
     gps_use = true;
     gps_lat = position.coords.latitude;
     gps_lng = position.coords.longitude;
-    console.log("Showlocation도 햇지?")
-    console.log(gps_lat)
-    console.log(gps_lng)
 
-    var currentPosition  = new kakao.maps.LatLng(gps_lat,gps_lng); 
+    var currentPosition  = new kakao.maps.LatLng(gps_lat,gps_lng); // 현재 위치정보로 위치객체 생성
             
     var marker = new kakao.maps.Marker({  
         map: map, 
         position: currentPosition, 
         image: new kakao.maps.MarkerImage('../static/icons/pin_current.png', new kakao.maps.Size(24, 24))
+        // 현재 위치는 빨간색 pin_current로 이미지 설정해둠
     }); 
 
-    marker.setMap(map);
-    map.setCenter(currentPosition);
-    console.log("위치 좌표 잡는게 나중에 아님?");
+    marker.setMap(map); // 내 위치 pin 박기
+    map.setCenter(currentPosition); // 내 위치를 중심 좌표로 이동
     map.setLevel(7);
-    bounds = map.getBounds();
+    bounds = map.getBounds(); // 새로 bound 가져오기.
 }
 
 
@@ -94,69 +74,62 @@ function errorHandler(error) {
         alert("위치를 반환할 수 없습니다.");
     }
     gps_use = false;
+    // 이 경우 bound는 처음에 설정한 값으로 유지됨 (변경 x)
 }
 
 
 
-// 2 부스 위치 찍기 -----------------------------
+// 2 부스 표시하기 -----------------------------
 
 // 주소 정보 가져오기
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
 
-
-// 주소로 좌표를 검색합니다
+// 일단 render로 넘어온 모든 booth들 안보이게 boothList에 등록
+// 이 부분은 추후 상의해봐야할듯 함.. 다 갖고오고 for문 돌려서 새로 표시만 하니까 로딩 너무 느려 ㅠ
 var boothList = document.getElementById('boothList');
 let total = boothList.childElementCount; // count booths    
-// var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 
-
-// var bounds = map.getBounds();
-// console.log("이게 젤 나중에여야 하는데.. 실행을 먼저 하나부다")
+// booth list하는 아코디언 dom
 var accList = document.getElementById('accordionList')
-// console.log(bounds.getSouthWest().toString())
-// console.log(bounds.getNorthEast().toString())
 
 async function for_pin(total){
     for (let i=0; i<total; i++) {
-        var pin = await pinnn(i);
+        var pin = await setbooth(i);
+        // pin은 다 찍고, list는 현재 중심좌표(내 위치든 기본값이든) 주변으로만 표시
     }
 }
 
 for_pin(total);
 
-function pinnn(i) {
-    // const element = document.getElementById(`mapdetail-${i}`);
-    let booth = boothList.children[i]
-    let address = booth.firstElementChild.dataset.loc
-    // console.log(address)
+function setbooth(i) {
+    let booth = boothList.children[i] // 특정 booth 정보 담은 객체
+    let address = booth.firstElementChild.dataset.loc // data-loc 형태로 넣어주었음
+    const name = booth.firstElementChild.dataset.name
+
+    // 등록 안되는거 예외처리
     if (address == "인천 미추홀구 숙골로87번길 5 5블럭 1층 40호") {
-        // console.log("yes")
         address = "인천 미추홀구 숙골로87번길 5";
     }
 
-    const name = booth.firstElementChild.dataset.name
-    // console.log(name)
-    // console.log(typeof name)
-    
+    // 특정 pin's infowindow 설정
     var content = '<div style="padding:2px;z-index:1;font-size:8px; text-align: center!important;">' + name + '</div>';
+    var infowindow = new kakao.maps.InfoWindow({zIndex:1}); // 새 info object
+    infowindow.setContent(content); // infowindow 내용
 
-    var infowindow = new kakao.maps.InfoWindow({zIndex:1});
-    infowindow.setContent(content);
-
+    // 주소 -> 좌표 변환 검색
     geocoder.addressSearch(address, function(result, status) {
-        // console.log(status)
         // 정상적으로 검색이 완료됐으면 
         if (status === kakao.maps.services.Status.OK) {
     
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-            // 경도 위도 값도 저장
+            // 경도 위도 값도 booth list에 저장
             booth.firstElementChild.setAttribute('data-lat', coords.getLat())
             booth.firstElementChild.setAttribute('data-lng', coords.getLng())
 
-            
             // 결과값으로 받은 위치를 마커로 표시합니다
+            // 마커 == pin
             var marker = new kakao.maps.Marker({
                 map: map,
                 position: coords,
@@ -165,8 +138,7 @@ function pinnn(i) {
             marker.setMap(map);
             // 지도에 핀은 일단 다 찍어놓기 
 
-            
-            infowindow.setPosition(coords);
+            infowindow.setPosition(coords); // 인포윈도우 달릴 위치 설정 (=해당 핀 좌표)
 
             (function(marker, infowindow) { // 파라미터
                 // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시합니다 
@@ -179,6 +151,9 @@ function pinnn(i) {
                     infowindow.close();
                 });
 
+                // 이 아래는 list에 mouseover시 하려고 했던 것
+                // 추후 디자인 logic 따라 수정 예정
+
                 // name_ele.onmouseover =  function () {
                 //     infowindow.open(map, marker);
                 // };
@@ -188,30 +163,24 @@ function pinnn(i) {
                 // };
                 // console.log("set hover func");
             })(marker, infowindow); // 실제 넘기는거
-
             
-            // 지도 boundary 안에 있는거만 list
+
+            // booth의 좌표가 현재 지도 boundary 안에 있는거면 list
             if (bounds.contain( coords )) {
-                printList(booth, accList); // list 표시하기                
+                printList(booth, accList); // list에 표시하기                
             }
-
-            else {
-                // console.log("not in map")
-            }  
-            
     
         }
-        else {
-            console.log("안돼?")
-            console.log(result)
+
+        else { // 주소->좌표 변환 실패한 경우
+            console.log("검색 실패. 주소가 잘 들어갔는지 확인해줄 것")
         }
     });
 
-     // marker 위에 infowindow 표시하기
-    return i;
-
 }
 
+
+// 3 초기 세팅 이후, 화면 변경에 따라 list 표시 다르게
 // 중심 좌표 움직였을 때
 kakao.maps.event.addListener(map, 'center_changed', findList);
 // 확대 축소 했을 때
@@ -220,9 +189,9 @@ kakao.maps.event.addListener(map, 'zoom_changed', findList)
 
 function findList() {
     accList.innerHTML = '';
-    // acc 리스트 초기화
+    // 이미 되어있던 acc 리스트 초기화
 
-    bounds = map.getBounds();
+    bounds = map.getBounds(); // 화면 변경되었으니 범위 다시 가져오고
 
     for (let i=0; i<total; i++) {
     
@@ -231,8 +200,9 @@ function findList() {
         let lng = booth.firstElementChild.dataset.lng
         boothcoord = new kakao.maps.LatLng(lat, lng)
         
+        // booth의 좌표가 현재 지도 boundary 안에 있는거면 list
         if (bounds.contain( boothcoord )) {
-            printList(booth, accList); // list 표시하기                
+            printList(booth, accList); // list에 표시하기                
         }
     
         else {
@@ -242,9 +212,11 @@ function findList() {
 
 }
 
+// 리스트에 매장 추가
+// 근데 넘 느려ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
 function printList(boothElement, AccElement) {
     
-
+    // 지도 내에 있는 booth의 정보 가져오기
     let name = boothElement.firstElementChild.dataset.name;
     let address = boothElement.firstElementChild.dataset.loc;
     const boothId = boothElement.dataset.id;
@@ -256,24 +228,19 @@ function printList(boothElement, AccElement) {
     const boxnum = parseInt(boothElement.firstElementChild.dataset.boxnum);
     const rating = parseFloat(boothElement.firstElementChild.dataset.rating);
     const likenum = boothElement.firstElementChild.dataset.likenum;
-
-    // console.log(typeof street) // str
-    // console.log(typeof likenum) // num
-    // console.log(typeof hour) // str
     
     let streetContent = ''
     let decoContent = ''
     let hourContent = ''
 
+    // detail 어떻게 표시될지 if문
     if (street) { streetContent = "매장점" }
     else { streetContent = "부스점" }
 
-    if (deco) { decoContent = "○" }
-    else { decoContent = "X" }
+    if (deco) { decoContent = "○" } // 소품 ㅇ
+    else { decoContent = "X" } // 소품 x
 
-    if (hour) { hourContent = hour }
-
-
+    if (hour) { hourContent = hour } // 시간 null 아닌 경우만 표시
 
     const newdiv = document.createElement('div');
     newdiv.setAttribute('class', 'accordion-item');
@@ -319,16 +286,5 @@ function printList(boothElement, AccElement) {
         </div>
     </div>`;
 
-    AccElement.append(newdiv);
+    AccElement.append(newdiv); // list추가
 }
-
-// 인포윈도우에 장소명을 표시합니다
-function displayInfowindow(marker, name) {
-    var content = '<div style="padding:5px;z-index:1;">' + name + '</div>';
-
-    infowindow.setContent(content);
-    infowindow.open(map, marker);
-}
-
-// mouse hover event
-
