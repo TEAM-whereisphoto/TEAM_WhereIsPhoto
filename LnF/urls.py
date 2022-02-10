d66637bfccaf981674e81fc7487c2303800967ac
YongbinKim <jinnem9025@daum.net>
2022-02-10 18:42:30 +0900

    [FEAT] LnF detail + 댓글

--- LnF/urls.py
+++ LnF/urls.py
@@ -6,5 +6,7 @@ app_name = "LnF"
 urlpatterns = [
     path("", view=views.list, name='list'),
     path("new/", view=views.new, name='new'),
-    # path("<int:pk/detail/", view=views.detail, name='detail'),
+    path("<int:pk>/detail/", view=views.detail, name='detail'),
+    path("<int:pk>/detail/add_comment/", view= views.add_comment, name="add_comment"),
+    path("<int:pk>/detail/del_comment/", view= views.del_comment, name="del_comment"),
 ]
--- LnF/views.py
+++ LnF/views.py
@@ -1,12 +1,17 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
+
+import LnF
 # from pytz import timezone
 from .models import *
 from .forms import *
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 from map.models import Booth
 from brand.models import Brand
 from user.models import User
+
+import json
 
 
 def list(request):
@@ -43,5 +48,35 @@ def new(request):
     ctx = {'form': form}
     return render(request, 'LnF/new.html', ctx)
 
+# pk: booth pk
+def detail(request, pk):
+    booth = get_object_or_404(Booth, id=pk)
+    posts = LnF_Post.objects.filter(booth = booth)
+    ctx = {'posts': posts, 'booth': booth}
+    return render(request, 'LnF/detail.html', context=ctx)
 
-# def detail(request):
+
+@csrf_exempt
+def add_comment(request, pk):
+    req = json.loads(request.body)
+    id = req['id']
+    type = req['type']
+    content = req['content']
+    
+    comment = Comment()
+    comment.post = LnF_Post.objects.get(id = id)
+    comment.content = content
+    comment.user = request.user
+    comment.time = timezone.now()
+    comment_id = comment.id
+    comment.save()
+    return JsonResponse({'id': id, 'type': type, 'content': content, 'comment-id': comment_id})
+
+@csrf_exempt
+def del_comment(request, pk):
+    req = json.loads(request.body)
+    comment_id = req['id']
+    
+    comment = get_object_or_404(Comment, id = comment_id)
+    comment.delete()
+    return JsonResponse({'id': comment_id})
