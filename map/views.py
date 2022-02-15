c6d5114a331d9c2ce23238a6927e887c0d5448b2
hyoung <ycat01@naver.com>
2022-02-16 01:07:07 +0900

    [FEAT] reivew content 목록

--- map/views.py
+++ map/views.py
@@ -24,7 +24,7 @@ def mymap(request):
     ctx = {'booths': booths} # 너무 많으면 여기서 booths[:10] 로 몇개만 뽑아도 됨!
     return render(request, 'map/mymap.html', context=ctx)
 
-def avg(request, pk): # 평균 별점 계산 함수
+def avg(pk): # 평균 별점 계산 함수
     booth = Booth.objects.get(id=pk)
     reviews = Review.objects.filter(booth = booth.pk)
 
@@ -49,7 +49,7 @@ def avg(request, pk): # 평균 별점 계산 함수
 def tag_count(pk):
     booth = Booth.objects.get(id=pk)  # id가 pk인 게시물 하나를 가져온다.
     reviews = Review.objects.filter(booth = booth.pk)
-    tag_list = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]
+    tag_list = [['clean', 0], ['prop', 0], ['booth', 0], ['iron', 0], ['street', 0]]
 
     for review in reviews:
         tags = []
@@ -65,12 +65,13 @@ def tag_count(pk):
                 tag_list[3][1] += 1
             elif tag == '로드점이에요':
                 tag_list[4][1] += 1
+    tag_list = sorted(tag_list, key= lambda x: -x[1])
     return tag_list
 
 def booth_brand(request, pk):
     booth = Booth.objects.get(id=pk)  # id가 pk인 게시물 하나를 가져온다.
     like = Liked.objects.all()
-    avg(request, pk)  # 왜 새로고침해야 뜨는거지
+    avg(pk)  # 왜 새로고침해야 뜨는거지
     brandname = booth.brand
     brand = Brand.objects.get(name=brandname)
     brand_list = []
@@ -97,15 +98,12 @@ def booth_detail(request,pk):
     booth = Booth.objects.get(id=pk)  # id가 pk인 게시물 하나를 가져온다.
     reviews = Review.objects.filter(booth = booth.pk)
     lnfs = LnF_Post.objects.filter(booth= booth.pk)
-    
+    avg(pk)
     brand = Brand.objects.all()
     brand_list = []
     brand_list = booth_brand(request, pk)
     tag_list = tag_count(pk)
-    tag_list = sorted(tag_list, key= lambda x: -x[1])
-    print(tag_list)
-
-    ctx = {'booth': booth, 'lnfs' : lnfs, 'reviews': reviews, 'brand_list': brand_list, 'pk': pk, 'tag_list':tag_list}
+    ctx = {'booth': booth, 'lnfs' : lnfs, 'reviews': reviews, 'brand_list': brand_list, 'pk': pk, 'tag_list' :tag_list}
     return render(request, template_name='map/booth_detail.html', context=ctx)
 
 def booth_review_list(request,pk):
@@ -125,7 +123,7 @@ def booth_review_create(request, pk):
                 post.booth = booth
                 post.user = user
                 post.boothid = pk
-                avg(request, pk)  # 왜 새로고침해야 뜨는거지
+                avg(pk)  # 왜 새로고침해야 뜨는거지
                 post.save()
                 return redirect('map:booth_review_list', pk)
         else:
@@ -133,14 +131,15 @@ def booth_review_create(request, pk):
         ctx = {'form': form}
         return render(request, template_name='map/review_create.html', context=ctx)
     else:
-        return redirect('user:login')
+        return redirect('user:login') #
 
 def review_detail(request, pk):  # request도 받고 몇번 인덱스인지 = pk를 받는다. 게시물 상세조
     review = Review.objects.get(id=pk)  # id가 pk인 게시물 하나를 가져온다
     tags = []
     tags = review.title
+    colors = review.color
 
-    ctx = {'review': review, 'pk':pk, 'tags':tags}  # template로 보내기 위해선 context를 만들어야한다.
+    ctx = {'review': review, 'pk':pk, 'tags':tags, 'colors': colors }  # template로 보내기 위해선 context를 만들어야한다.
     return render(request, template_name='map/review_detail.html', context=ctx)
 
 
@@ -161,6 +160,8 @@ def review_update(request, pk):
 def review_delete(request, pk):
     review = get_object_or_404(Review, id=pk)
     booth_id = review.boothid
+    booth = Booth.objects.get(id=booth_id)  # id가 pk인 게시물 하나를 가져온다.
+    booth.review_number -= 1
     review.delete()
     return redirect('map:booth_review_list', booth_id)
 
