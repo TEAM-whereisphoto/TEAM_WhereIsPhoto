cee1eb9c848bc0201e6c83b527bd691f7badfb39
Dasom Ahn <lizzy0521@naver.com>
2022-02-18 18:16:43 +0900

    [FIX] tag

--- static/js/map/like.js
+++ static/js/map/like.js
@@ -26,3 +26,16 @@ const onClickAlert = () =>{
     alert('로그인을 먼저 해주세요.!')
     window.location.href ='/user/login'
 }
+
+// ------------------------------------------------------------
+// tag fill 채우기
+document.addEventListener("DOMContentLoaded", function(event){
+    // your code here
+    const tagfills = this.getElementsByClassName("tag__fill")
+    for (let eachtag of tagfills) {
+        let width = eachtag.dataset.num / eachtag.dataset.reviews * 100
+        console.log(width)
+        eachtag.setAttribute("style", `width: ${width}%`)
+    }
+    
+});
