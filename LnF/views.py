1b96dba34f43f7186d96ff3c579447ad14dfaa42
Dasom Ahn <lizzy0521@naver.com>
2022-02-19 15:19:00 +0900

    [FIX] LnF ajax 필터

--- LnF/views.py
+++ LnF/views.py
@@ -165,7 +165,7 @@ def tag(request):
         else:
             img = ""
 
-        resList.append({'booth_name': post.booth.name, 'booth_id': post.booth.id ,'user': post.user.username, 'timeString': post.timeString, 'time': post.time.strftime('%m월 %d일'), 'content': post.content, 'img': img, 'tag': post.tag})
+        resList.append({'id': post.id, 'booth_name': post.booth.name, 'booth_id': post.booth.id ,'user': post.user.username, 'timeString': post.timeString, 'time': post.time.strftime('%m월 %d일'), 'content': post.content, 'img': img, 'tag': post.tag})
     return JsonResponse({'resList': resList})
 
  
--- static/js/LnF/filter.js
+++ static/js/LnF/filter.js
@@ -2,109 +2,74 @@ const requestTag = new XMLHttpRequest();
 
 const checkboxes = document.querySelectorAll("input[type=checkbox][name=tag]")
 const checkDict = {'분실': true, '보관': true}
-
+console.log(checkboxes)
 checkboxes.forEach(function(checkbox){
     checkbox.addEventListener("change", function(){
         const url = "tag/"
-        const value = checkbox.value;
-        const checkedIdx = (checkbox.checked == true)
-        const query = document.getElementById('query').value
+        var value = checkbox.value;
+        var checkedIdx = (checkbox.checked == true)
         checkDict[value] = checkedIdx
+        
+        const query = document.getElementById('query').value
         requestTag.open("POST", url, true);
         requestTag.setRequestHeader(
             "Content-Type", "application/x-www-form-urlencoded"
         );
         requestTag.send(JSON.stringify({'분실': checkDict['분실'], '보관': checkDict['보관'], 'query': query }))
-        })
+    })
     
 })
 const filterByTag = () =>{
     if (requestTag.status < 400){
-        const {resList, query} = JSON.parse(requestTag.response)
+        const {resList} = JSON.parse(requestTag.response)
 
         const postList = document.querySelectorAll('#postList > div > div');
         for (const div of postList){
             div.remove();
         }
+        // 목록 다 삭제
+
         const postListSection = document.querySelector('#postList');
         
         for (const post of resList){
             const container = document.createElement('div')
-            container.setAttribute
+
+            const timeString = post.timeString
+            if (timeString == false){
+                var timetext = post.time
+            }
+            else{
+                var timetext = post.timeString
+            }
 
             container.innerHTML=`
             <div class="post shadow p-3">
                 <div class="post__title">
                     <span class="post__${ post.tag }">${post.tag}</span>
-                    <a href="/LnF/${post.booth_id}/detail/">${post.booth.name}</a>
+                    <a href="/LnF/${post.id}/post_detail/">${post.booth_name}</a>
                 </div>
                 <div style="height: .5rem;"></div>
-                <!-- post content -->
                 <div class="post_content">
                     ${post.content} <br>
-                    {% if post.img %}
-                    <img src="${post.img.url}" alt="" width="300"> 
-                {% endif %}
                 </div>
                 <div style="height: .5rem;"></div>
-                <!-- post footer -->
                 <div class="post__footer">
                     <div>
-                        ${post.user.username}}<br>
+                        ${post.user}<br>
                     </div>
                     <div>
-                        {% if post.timeString == False %}
-                        {{post.time|date:'m월 d일'}}
-                        {% else %}
-                        {{post.timeString}}
-                        {% endif %}
+                        ${timetext}
                     </div>
                 </div>          
-            </div>
-
-            <div>
-            <a href="/LnF/${post.booth_id}/detail/">부스명: ${post.booth_name}</a>
-            <div>
-                태그: ${post.tag}
-            </div>
-            <div>
-                내용: ${post.content}
-            </div>
-            <div>
-                작성자: ${post.user}<br>
-            </div>
-            `
+            </div>`
             
-            const timeDiv = document.createElement('div')
-            const imgDiv = document.createElement('div')
-
-            const timeString = post.timeString
-            if (timeString == false){
-                timeDiv.innerHTML = '작성시간: '+ post.time
-            }
-            else{
-                timeDiv.innerHTML = '작성시간: ' + post.timeString
-            }
-
-            if (post.img != ''){
-                const img = document.createElement('img')
-                img.setAttribute("src", post.img)
-                img.setAttribute("width", 300)
-                imgDiv.append(img)
-            }
-
-
-            container.append(timeDiv)
-            container.append(imgDiv)
-
-            postListSection.append(container)
+            postListSection.children[0].append(container)
         }
-}
+    }
 }
 requestTag.onreadystatechange = () => {
     if(requestTag.readyState === XMLHttpRequest.DONE){
         filterByTag();
-    
     }
 }
 
